69cc3aecefe718a75bbcf78d4d14362f103b09b4
WISUNGWON <tkrhkaos12@naver.com>
2020-02-27 23:53:02 +0900

    Add Java Files

--- src/inflearn/chapter2/recurtion/RecursionEx05.java
+++ src/inflearn/chapter2/recurtion/RecursionEx05.java
@@ -6,7 +6,7 @@ public class RecursionEx05 {
   public static void main(String[] args) {
 
   }
-  // ex 1) 문자열의 길이 계산
+ 
   public static int length(String str) {
     if (str.equals(""))
       return 0; // base case
@@ -14,17 +14,17 @@ public class RecursionEx05 {
       return 1 + length(str.substring(1));
   }
   
-  // ex 2) 문자열의 프린트
+  
   public static void printChars(String str) {
     if (str.length() == 0)
       return;
     else {
-      System.out.print(str.charAt(0)); //charAt(index) 문자열의 index에 해당하는 문자를 return.
+      System.out.print(str.charAt(0)); .
       printChars(str.substring(1));
     }
   }
   
-  // ex 3) 문자열을 뒤집어 프린트 
+ 
   public static void printCharsReverse(String str) {
     if (str.length() == 0)
       return;
@@ -33,7 +33,7 @@ public class RecursionEx05 {
       System.out.print(str.charAt(0));
     }
   }
-  // ex 4) 2진수로 변환하여 출력
+
   public void printInBinary(int n) {
     if (n < 2)
       System.out.print(n);
@@ -43,7 +43,7 @@ public class RecursionEx05 {
     }
   }
   
-  // ex 5) 배열의 합 구하기
+
   public static int sum(int n, int[] data) {
     if (n <= 0) // base case
       return 0;
@@ -51,8 +51,8 @@ public class RecursionEx05 {
       return sum(n-1, data) + data[n-1]; // recursive case
   }
   
-  // ex 6) 데이터파일로 부터 n개의 정수 읽어오기
-  public void readFrom(int n, int[] data, Scanner in) { // Scanner in : data 파일을 참조. 
+
+  public void readFrom(int n, int[] data, Scanner in) {  
     if (n == 0)
       return;
     else {
@@ -60,6 +60,7 @@ public class RecursionEx05 {
       data[n-1] = in.nextInt();
     }
   }
+ 
   
 }
 
