4dac694149fd9f19db174377bddacc551d4b9e55
WISUNGWON <tkrhkaos12@naver.com>
2020-04-09 00:39:13 +0900

    Add Files

---
+++ src/algorithm/dongbin/sort/CountingSort.java
@@ -0,0 +1,32 @@
+package algorithm.dongbin.sort;
+
+public class CountingSort {
+
+    public static void main(String[] args) {
+        int temp;
+        int[] count = new int[5];
+        int[] array = {
+                1, 3, 2, 4, 3, 2, 5, 3, 1, 2,
+                3, 4, 4, 3, 5, 1, 2, 3, 5, 2,
+                3, 1, 4, 3, 5, 1, 2, 1, 1, 1
+        };
+        
+        for (int i = 0; i < count.length; i++) {
+            count[i] = 0;
+        }
+        
+        for (int i = 0; i < 30; i++) {
+            count[array[i] - 1]++;
+        }
+        
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] != 0) {
+                for (int j = 0; j < count[i]; j++) {
+                    System.out.printf("%d", i + 1);
+                }
+            }
+        }
+
+    }
+
+}
---
+++ src/sort/backjoon/WordsSorting.java
@@ -0,0 +1,15 @@
+package sort.backjoon;
+
+//백준 1181 단어 정렬
+public class WordsSorting {
+
+    public static void main(String[] args) {
+        
+        String a = "it";
+        String b = "ia";
+        
+        System.out.println(a > b);
+
+    }
+
+}
