4fe1be2a2f43e6f426b09aeccd1fe51118239c40
WISUNGWON <tkrhkaos12@naver.com>
2020-05-15 11:41:54 +0900

    Add Algo!

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -7,23 +7,7 @@ public class Main {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-
-        
-        while (t != 0) {
-            int r = sc.nextInt();
-            String s = sc.next();
-            for (int i = 0; i < s.length(); i++) {
-                for (int j = 0; j < r; j++) {
-                    System.out.print(s.substring(i, i + 1));
-                }
-            }
-            System.out.println();
-            t--;
-        }
-        
-       
-        sc.close();
+        int n = sc.nextInt(), k = sc.nextInt();
         
 
     }
