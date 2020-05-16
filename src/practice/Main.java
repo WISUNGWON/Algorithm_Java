516c6cb02f80e028602b9f5fba06b64b4e7d8ede
WISUNGWON <tkrhkaos12@naver.com>
2020-05-16 23:17:04 +0900

    Add algo!

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -7,7 +7,24 @@ public class Main {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
+       
+        int result = 0;
         int n = sc.nextInt(), k = sc.nextInt();
+        int[] arrCoin = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            arrCoin[i] = sc.nextInt();
+        }
+        
+        for (int i = n - 1; i >= 0; i--) {
+            result += k / arrCoin[i];
+            k = k % arrCoin[i];
+        }
+        
+        System.out.println(result);
+        
+        
+        
         
 
     }
