3659e3f02b0729bfb9d42c0b1c641164aa00e972
WISUNGWON <tkrhkaos12@naver.com>
2020-05-23 21:47:26 +0900

    Add algo!

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -5,21 +5,14 @@ import java.util.Scanner;
 public class Main {
     
         public static void main(String[] args) {
+           
             Scanner sc = new Scanner(System.in);
-            String a = sc.nextLine(), b = sc.nextLine();
-            int[][] dp = new int[b.length() + 1][a.length() + 1];
+            int n = sc.nextInt();
+            int[][] dp = new int[n + 1][3];
             
-            for (int i = 1; i <= b.length(); i++) {
-                for (int j = 1; j <= a.length(); j++) {
-                    if (b.charAt(i - 1) != a.charAt(j - 1)) {
-                        dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
-                    } else {
-                        dp[i][j] = dp[i - 1][j - 1] + 1;
-                    }
-                }
-            }
-            
-            System.out.println(dp[b.length()][a.length()]);
+            dp[1][0] = 1;
+            dp[1][1] = 1;
+            dp[1][0] = 1;
                     
         }
     
