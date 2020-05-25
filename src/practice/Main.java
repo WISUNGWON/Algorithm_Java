c7a18780c4f0b64817ffcd442d3affbae7e57616
WISUNGWON <tkrhkaos12@naver.com>
2020-05-25 18:09:23 +0900

    Add algo

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -8,11 +8,27 @@ public class Main {
            
             Scanner sc = new Scanner(System.in);
             int n = sc.nextInt();
-            int[][] dp = new int[n + 1][3];
+            int k = sc.nextInt();
             
-            dp[1][0] = 1;
-            dp[1][1] = 1;
-            dp[1][0] = 1;
+            int[][] dp = new int[n + 1][k + 1];
+            int w[] = new int[n + 1];
+            int v[] = new int[n + 1];
+            
+            for (int i = 1; i <= n; i++) {
+                w[i] = sc.nextInt();
+                v[i] = sc.nextInt();
+            }
+            
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= k; j++) {
+                    dp[i][j] = dp[i - 1][j];
+                    if (j - w[i] >= 0) {
+                        dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
+                    }
+                }
+            }
+            
+            System.out.println(dp[n][k]);
                     
         }
     
