3b1dc9da5084e2933ed696092a8be536a3ce776b
WISUNGWON <tkrhkaos12@naver.com>
2020-06-07 22:35:57 +0900

    Add algo!

--- src/dp3/backjoon/Coin.java
+++ src/dp3/backjoon/Coin.java
@@ -1,10 +1,12 @@
 package dp3.backjoon;
 
+import java.util.Scanner;
+
 // 9084 µ¿Àü
 public class Coin {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
 
     }
 
