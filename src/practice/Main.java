8149ccb6c7e8955d818abeb4b566cd2af40a06f4
WISUNGWON <tkrhkaos12@naver.com>
2020-06-12 23:53:37 +0900

    Add algo

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -7,7 +7,7 @@ public class Main {
     
         public static void main(String[] args) throws IOException {
            
-     
+            Scanner sc = new Scanner(System.in);
             
         }
     
