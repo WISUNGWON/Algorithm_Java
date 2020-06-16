48d5e497e0f792bc1763c0e672c422fde2843d9f
WISUNGWON <tkrhkaos12@naver.com>
2020-06-16 21:08:51 +0900

    Add algo!

--- src/all/backjoon/Clock.java
+++ src/all/backjoon/Clock.java
@@ -1,12 +1,30 @@
 package all.backjoon;
 
+import java.util.Scanner;
+
+//2884 알람시계
 public class Clock {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
         Scanner sc = new Scanner(System.in);
         
-        int i = 0;
+        int H = sc.nextInt();
+        int M = sc.nextInt();
+        sc.close(); 
+        
+        if (M < 45) {
+            H--;
+            M = 60 - (45 - M);
+            if (H < 0) {
+                H = 23;
+            }
+            
+            System.out.println(H + " " + M);
+        }
+        
+        else {
+            System.out.println(H + " " + (M - 45));
+        }
     }
 
 }
