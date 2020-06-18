f9094d3800146abc5fa68e3444790f1fa7912eea
WISUNGWON <tkrhkaos12@naver.com>
2020-06-18 22:40:29 +0900

    Add algo!

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -9,25 +9,7 @@ public class Main {
            
             Scanner sc = new Scanner(System.in);
             
-            long A = sc.nextLong();
-            long B = sc.nextLong();
-            long C = sc.nextLong();
-            
-            System.out.println(power(A % C, B, C));
-            
-        }
-        
-        private static long power(long a, long b, long c) {
-            if (b == 1) return a;
-            
-            long temp = power(a, b / 2, c) % c;
-            
-            if (b % 2 == 0) {
-                return (temp * temp) % c;
-            } else {
-                return (((temp * temp) % c) * a) % c;
-            }
-        }
+        }   
  
 }
 
