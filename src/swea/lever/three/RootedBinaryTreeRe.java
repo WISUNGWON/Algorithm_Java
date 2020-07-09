839373a657c31cf0488800b1aa0c8712aca030d7
WISUNGWON <tkrhkaos12@naver.com>
2020-07-09 20:39:18 +0900

    One Day One SWEA

---
+++ src/swea/lever/three/RootedBinaryTreeRe.java
@@ -0,0 +1,46 @@
+package swea.lever.three;
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class RootedBinaryTreeRe {
+
+//7985. Rooted Binary Tree 재구성
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int k = Integer.parseInt(br.readLine());
+            String[] nums = br.readLine().split(" ");
+            int degree = 0;
+            int len = nums.length;
+            int sqrt = 1;
+            while (sqrt < len + 1) {
+                sqrt *= 2;
+                degree++;
+            }
+            
+            bw.write("#"+test_case+" ");
+            for (int i = degree; i > 0; i--) {
+                int idx = (int) Math.pow(2, i - 1);
+                int interval = idx*2;
+                idx--;
+                bw.write(nums[idx]);
+                while (idx + interval < len) {
+                    idx += interval;
+                    bw.write(" "+nums[idx]);
+                }
+                bw.write('\n');
+            }
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
